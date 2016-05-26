9071e94d6fb60bfeffc04b64340c84053f6e44f5
emmanuel <emmanueljucaban@gmail.com>
2016-05-26 03:20:23 +0000

    Added devise and user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'http://nomster-emmanuel-jucaban.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
